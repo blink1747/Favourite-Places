59799f2eda1417cdca8b7e21125f4615bc2260f0
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-12 18:01:16 -0500

    Final Code

---
+++ Pin+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Pin+CoreDataClass.swift
+//  Favourite Places
+//
+//  Created by IMCS on 8/12/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pin)
+public class Pin: NSManagedObject {
+
+}
